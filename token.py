772ed87352663c1463b4a449a4953fc9e50d0323
joshi-rohit <rohit@pingal.co>
2017-10-16 18:27:53 -0400

    keys

---
+++ token.py
@@ -0,0 +1,16 @@
+class Keys(object):
+    def __init__(self):
+        pass
+
+    def generate_keys(self):
+        pass
+
+    def store_keys(self):
+        pass
+
+class Token(object):
+    def __init__(self):
+        pass
+
+    def generate_tokens(self):
+        pass
