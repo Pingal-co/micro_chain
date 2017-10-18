6c4345da074d83e1def846b3429d93f6f3090b01
joshi-rohit <rohit@pingal.co>
2017-10-17 20:01:17 -0400

    validate transactions

--- transaction.py
+++ transaction.py
@@ -2,35 +2,51 @@ from utils import bin_dbl_sha256
 import time
 
 
-
 class TxInput(object):
+    def __init__(self, tx):
+        """
+        a data type of the form (previous_tx_hash, output_index)
+        outpoint:  32-byte TXID and a 4-byte output index number 
+        """
+        self.previous_tx_hash = tx["previous_tx_hash"]
+        self.output_index = tx["output_index"]
+
+class TxOutput(object):
+    def __init__(self, tx):
+        """
+        a data type of the form (recipient, value)
+        """
+        self.recipient = tx["recipient"], 
+        self.amount = tx["amount"]
+
+class TxInputs(object):
     def __init__(self):
         """
         a list of list of the form [[previous_tx_hash, output_index]]
         outpoint:  32-byte TXID and a 4-byte output index number 
         """
-        self.inputs = []
+        self.data = []
 
     def add(self, txs):
         for tx in txs:
-            self.inputs.append([tx["previous_tx_hash"], tx["output_index"])
+            self.data.append(TxInput(tx))
 
     def serialize(self):
-        return "".join([str(inp[0]) + str(inp[1]) for inp in self.inputs]) 
+        return "".join([str(d.previous_tx_hash) + str(d.output_index) for d in self.data]) 
 
-class TxOutput(object):
-    def __init__(self, outs=[]):
+class TxOutputs(object):
+    def __init__(self):
         """
         a list of list of the form [[recipient, value]]
         """
-        self.outputs = outs
+        self.data = []
 
     def add(self, recipient, amount):
         for tx in txs:
-            self.outputs.append([tx["recipient"], tx["amount"])
+            self.data.append(TxOutput(tx))
 
     def serialize(self):
-        return "".join([str(out[0]) + str(out[1]) for out in self.outputs]) 
+        return "".join([str(d.recipient) + str(d.amount) for d in self.data]) 
 
 class Transaction(object):
     def __init__(self, wallet, txs):
@@ -65,28 +81,7 @@ class Transaction(object):
         # SHA256(SHA256())
         return bin_dbl_sha256(s)
 
-    
-    def verify_transaction(self):
-        pass
-
-
-    def validate_transaction(self):
-        """
-        Holds the logic for verifying a transaction.
-        Based on: https://en.bitcoin.it/wiki/Protocol_rules
-        """
-        if not self.header.senderKey:
-            return False
-        if not self.header.recipient:
-            return False
-        if not self.header.amount:
-            return False
-        if not self.sign: 
-            return False
-        # validate signature of the sender
-
-        return True
-
+                
     def serialize(self):
         """
         Packs a dictionary with transaction data in accordance with Bitcoin's transaction format:
@@ -94,3 +89,64 @@ class Transaction(object):
         """
         pass
 
+def validate_transaction(tx):
+    """
+    Holds the logic for verifying a transaction.
+    Based on: https://en.bitcoin.it/wiki/Protocol_rules
+    """
+    if invalid_format(tx):
+        False
+        
+    # validate signature of the sender
+    if tx_already_exists(tx):
+        False
+
+    if ensure_inputs_not_already_spent(tx):
+        False
+
+    if ensure_inputs_ownership(tx):
+        return False
+
+    if ensure_inputs_sum_greater_than_outputs(tx):
+        return False
+
+    if ensure_public_key_ownsership(tx):
+        return False
+
+    return True
+
+def invalid_format(tx):
+    if len(tx.inputs) <= 0 :
+        return True
+    if len(tx.outputs) <= 0 :
+        return True 
+    if not is_sha256(tx.hash):
+        return True 
+    if not tx.signature:  
+        return True
+
+    for d in tx.inputs.data:
+        valid = is_sha256(d.previous_tx_hash) and  d.output_index >= 0):
+        if not valid:
+            return True
+
+    for d in tx.outputs.data:
+        valid = is_ripemd160(d.recipient) and d.amount > 0
+        if not valid:
+            return True
+
+
+def tx_already_exists(tx):
+    return False
+
+def ensure_inputs_not_already_spent(tx):
+    return False
+
+def ensure_inputs_ownership(tx):
+    return False
+
+def ensure_inputs_sum_greater_than_outputs(tx):
+    return False
+
+def ensure_public_key_ownsership(tx):
+    return False
